fc4ce2771cb2d4d61737d239175b3fdc7ed60dd4
Minh Nhựt <minhnhuttn@users.noreply.github.com>
2020-06-17 14:06:35 +0700

    create theme

---
+++ rovinacoffee/functions.php
@@ -0,0 +1,23 @@
+<?php
+add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
+add_theme_support('automatic-feed-links');
+register_nav_menus(array(
+    'primary-menu' => 'Primary menu',
+));
+
+function rovina_coffee_enqueue_scripts()
+{
+    $theme_version = wp_get_theme()->get('Version');
+
+    //add styles
+    wp_enqueue_style('bootstrap', get_theme_file_uri('/assets/lib/bootstrap/css/bootstrap.min.css'), array(), 'v4.0.0', 'all');
+    wp_enqueue_style('style', get_theme_file_uri('/assets/css/style.css'), array(), $theme_version, 'all');
+
+    //add scripts
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/lib/bootstrap/js/bootstrap.min.js'), array(), 'v4.0.0', true);
+    wp_enqueue_script('script', get_theme_file_uri('/assets/js/script.js'), array('jquery'), $theme_version, true);
+}
+
+add_action('wp_enqueue_scripts', 'rovina_coffee_enqueue_scripts');
---
+++ rovinacoffee/search.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * StartPress Framework.
+ *
+ * The template for displaying search.
+ *
+ * User: nhutfs
+ * Date: 31/10/2019
+ *
+ * @link https://startpress.net
+ *
+ * @package WordPress
+ * @subpackage StartPress Framework
+ * @since 2.0.2
+ */
+
+startpress_search();
