292155e20d14563106b38b9952c4bbecb0695c92
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-06-18 13:21:12 +0700

    Góc Trãi Nghiệm  + Reponsive

--- wordpress/wp-content/themes/rovinacoffee/templates/template-experience.php
+++ wordpress/wp-content/themes/rovinacoffee/templates/template-experience.php
@@ -3,6 +3,7 @@
  * Template Name: Góc trãi nghiệm
  */
 get_header();
+$urlTheme = get_template_directory_uri();
 ?>
 
     <div class="template-experience">
@@ -22,20 +23,23 @@ get_header();
             <div class="tab-content">
                 <div class="tab-pane active" id="tab1">
                     <div class="experience-top">
-                        <div class="experience-top-item d-flex align-items-end justify-content-between">
+                        <div class="experience-top-item d-flex align-items-end justify-content-between flex-wrap">
                             <div class="experience-information">
-                                <h2 class="title-experience text-uppercase"><a href="#" title="" class="color-experience">Lợi thế khi nhượng
+                                <h2 class="title-experience text-uppercase"><a href="#" title=""
+                                                                               class="color-experience">Lợi thế khi
+                                        nhượng
                                         quyền thương hiệu ROVINA COFFEE</a></h2>
                                 <p class="description">
                                     Lợi thế khi nhượng quyền thương hiệu ROVINA COFFEE Nhiều chủ đầu tư đã hỏi chúng tôi
                                     “nhượng quyền thương hiệu ROVINA COFFEE” so với việc thuê kiến trúc sư thiết kế quán
                                     mới sau đó tự học pha chế thì có gì khác nhau không? Chúng tôi muốn nhấn mạnh...
                                 </p>
-                                <a href="#" title="Xem thêm" class="viewmore d-flex justify-content-center align-items-center">XEM THÊM</a>
+                                <a href="#" title="Xem thêm"
+                                   class="viewmore d-flex justify-content-center align-items-center">XEM THÊM</a>
                             </div>
                             <div class="experience-image">
                                 <a href="#" class="color-experience" title="Title">
-                                    <img src="<?= get_template_directory_uri() ?>/assets/images/images-experience.jpg"
+                                    <img src="<?= $urlTheme ?>/assets/images/images-experience.jpg"
                                          alt="">
                                 </a>
                             </div>
@@ -43,8 +47,49 @@ get_header();
                     </div>
                 </div>
                 <div class="tab-pane fade" id="tab2">
-
+                    <div class="experience-top">
+                        <div class="experience-top-item d-flex align-items-end justify-content-between">
+                            <div class="experience-information">
+                                <h2 class="title-experience text-uppercase"><a href="#" title=""
+                                                                               class="color-experience">Lợi thế khi
+                                        nhượng
+                                        quyền thương hiệu ROVINA COFFEE</a></h2>
+                                <p class="description">
+                                    Lợi thế khi nhượng quyền thương hiệu ROVINA COFFEE Nhiều chủ đầu tư đã hỏi chúng tôi
+                                    “nhượng quyền thương hiệu ROVINA COFFEE” so với việc thuê kiến trúc sư thiết kế quán
+                                    mới sau đó tự học pha chế thì có gì khác nhau không? Chúng tôi muốn nhấn mạnh...
+                                </p>
+                                <a href="#" title="Xem thêm"
+                                   class="viewmore d-flex justify-content-center align-items-center">XEM THÊM</a>
+                            </div>
+                            <div class="experience-image">
+                                <a href="#" class="color-experience" title="Title">
+                                    <img src="<?= $urlTheme ?>/assets/images/images-experience.jpg"
+                                         alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+
+            <div class="list-news d-flex flex-wrap justify-content-between">
+                <?php for ($i = 1; $i < 7; $i++): ?>
+                    <div class="news-item news-item-experience">
+                        <a href="#" title="" class="box-img">
+                            <img src="<?= $urlTheme ?>/assets/images/news-experience-<?= $i ?>.jpg" alt="">
+                        </a>
+                        <div class="box-information-experience">
+                            <h3 class="title"><a href="#" title="">Lorem ipsum dolor sit amet, consectetur adipiscing
+                                    elittemp</a></h3>
+                            <div class="description">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                                Risus commod.
+                            </div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
             </div>
         </div>
     </div>
