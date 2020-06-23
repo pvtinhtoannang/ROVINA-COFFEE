ab2b36c10bf5772ed6045750fdf77adab2806a5d
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-06-23 14:09:28 +0700

    product + reponsive

---
+++ wordpress/wp-content/themes/rovinacoffee/templates/template-get-coffee-machine.php
@@ -0,0 +1,48 @@
+<section class="template-get-coffee-machine">
+    <div class="container">
+        <fieldset>
+            <legend class="text-center">
+                <h2>THÔNG TIN MUA MÁY CAFE</h2>
+                <p>Chỉ áp dụng tại quán thuộc khu vực TP.HCM</p>
+            </legend>
+            <form action="">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <input type="text" class="input-type-1 stt" value="" name="stt" placeholder="STT">
+                        <input type="text" class="input-type-1 name" value="" name="stt" placeholder="Tên sản phẩm">
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <input type="number" class="input-type-1 qty" value="" name="stt" placeholder="Số lượng">
+                        <input type="text" class="input-type-1 price" value="" name="stt" placeholder="Giá tiền">
+                    </div>
+
+                    <div class="col-xs-12 col-sm-6">
+                        <input type="text" class="input-type-2" value="" name="stt" placeholder="Tên quán">
+                        <select class="input-type-2"  name="" id="">
+                            <option value="">Thành phố Hồ Chí Minh</option>
+                            <option value="">Thành phố Cần Thơ</option>
+                        </select>
+                        <input type="text" placeholder="Điện thoại" class="input-type-2">
+                        <select class="input-type-2"  name="" id="">
+                            <option value="">Cá nhân</option>
+                            <option value="">Doanh nghiệp</option>
+                        </select>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <input type="text" class="input-type-2" value="" name="stt" placeholder="Địa chỉ quán của bạn">
+                        <select class="input-type-2"  name="" id="">
+                            <option value="">Quận 3</option>
+                            <option value="">Quận 4</option>
+                        </select>
+                        <input type="text" placeholder="Email" class="input-type-2">
+                        <input type="text" placeholder="Số lượng máy" class="input-type-2">
+                    </div>
+                    <div class="col-xs-12 col-sm-12">
+                        <textarea name="" id="" class="input-type-1" cols="30" rows="10"></textarea>
+                    </div>
+                </div>
+                <button class="btn-submit"><span>ĐẶT HÀNG</span></button>
+            </form>
+        </fieldset>
+    </div>
+</section>
---
+++ wordpress/wp-content/themes/rovinacoffee/templates/template-product.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Sản phẩm
+ */
+get_header();
+$urlTheme = get_template_directory_uri();
+get_template_part('templates/template-after-header');
+
+?>
+    <section class="template-product">
+        <div class="container">
+            <div class="list-product row">
+                <?php for ($i = 1; $i < 9; $i++): ?>
+                    <div class="product-item col-xs-12 col-sm-6 col-md-3">
+                        <div class="information-product text-center">
+                            <div class="box-img">
+                                <a href="#">
+                                    <img src="<?= $urlTheme ?>/assets/images/product-item-<?= $i ?>.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="information">
+                                <h3 class="title"><a href="#">Cafe Rovina123</a></h3>
+                                <p class="description">25.000 <sup>đ</sup>/100g</p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+            <div class="pagination-wrapper">
+                <div class="wp-pagenavi d-flex justify-content-center flex-wrap" role="navigation">
+                    <span class="pages">Trang 1 trên 3</span><span
+                        aria-current="page" class="current">1</span><a class="page larger" title="Page 2"
+                                                                       href="http://hiengiang.vn/san-pham/page/2/">2</a><a
+                        class="page larger" title="Page 3" href="http://hiengiang.vn/san-pham/page/3/">3</a>
+                    <a class="nextpostslink" rel="next"
+                       href="http://hiengiang.vn/san-pham/page/2/">»</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+get_template_part('templates/template-get-coffee-machine');
+get_footer();
